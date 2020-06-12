b3d0770810a0f9688dcefe76fe15438a6df34937
Wyatt Spear <wspear@cs.uoregon.edu>
2020-06-12 11:51:34 -0700

    Added initial product list sorting functionality

--- bin/e4sListPage.py
+++ bin/e4sListPage.py
@@ -23,12 +23,10 @@ introListBlock='''<!DOCTYPE html>
 	<title>E4S Products</title>
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
-	<link href="style.css" rel="stylesheet">
-
 
 <style>
 .product_button {
-  background-color: #777;
+  background-color: #555;
   color: white;
   cursor: pointer;
   padding: 18px;
@@ -40,23 +38,13 @@ introListBlock='''<!DOCTYPE html>
 }
 
 .active, .product_button:hover {
-  background-color: #555;
+  background-color: #222;
 }
 
 </style>
 </head>
 
-<body>
-
-<script src="jquery.js"></script>
-<script>
-$(function () {
-    $(".load-html").each(function () {
-        $(this).load(this.dataset.source);
-    });
-});
-</script>
-
+<body style="background-color:#777">
 
 
 <div class="wrapper">
@@ -72,9 +60,75 @@ $(function () {
 				<H2>E4S Products</h2>
 
 ***TIMESTAMP***<br>
-<hr>'''
+<hr>
+<table id="productTable">
+<tr>
+<th onclick="sortTable(0)">Name</th>
+</tr>
+'''
 
-introCloseBlock='''			</main><!-- .content -->
+introCloseBlock='''			
+</table>
+<script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("productTable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>
+
+
+</main><!-- .content -->
 		</div><!-- .container-->
 
 	</div><!-- .middle-->
@@ -87,8 +141,10 @@ introHeadingBlock='''<h3>***HEADING***</h3>
 '''
 
 introLinkBlock='''<!-- This is a single product entry. To add more copy and edit from here... -->
+<tr><td>
 <button class="product_button"><b><a href="***LOWNAME***.html">***CAPNAME***</a></b>
 </button>
+</td></tr>
 <!-- ...to here (product entry end) -->'''
 
 
